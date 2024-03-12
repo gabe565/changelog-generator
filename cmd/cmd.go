0d5613eef61c11d5ea86c6edce7092977e106e97
Gabe Cook <gabe565@gmail.com>
2024-03-12 00:18:45 -0500

    fix: Fix first tag pulled instead of last

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gabe565/changelog-generator/internal/config"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/spf13/cobra"
 )
 
@@ -45,8 +46,11 @@ func run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	previous, err := tags.Next()
-	if err != nil && !errors.Is(err, io.EOF) {
+	var previous *plumbing.Reference
+	if err := tags.ForEach(func(reference *plumbing.Reference) error {
+		previous = reference
+		return nil
+	}); err != nil && !errors.Is(err, io.EOF) {
 		return err
 	}
 
