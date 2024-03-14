094ee05d25bdf1cbb05712374aee234eda733065
Gabe Cook <gabe565@gmail.com>
2024-03-14 02:23:01 -0500

    feat(cmd): Add `--version` flag

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ import (
 	"github.com/gabe565/changelog-generator/cmd"
 )
 
+var (
+	version = "beta"
+	commit  = ""
+)
+
 func main() {
-	if err := cmd.New().Execute(); err != nil {
+	if err := cmd.New(version, commit).Execute(); err != nil {
 		os.Exit(1)
 	}
 }
